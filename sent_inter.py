2c581aff52390dbb570fa65694741c6dca3b6faa
cynxchen <cynthia.chen678@gmail.com>
2018-05-15 18:21:30 -0400

    improved interation

--- sent_inter.py
+++ sent_inter.py
@@ -4,11 +4,16 @@ from six.moves import input
 
 def compare(args):
     while (True):
+        print ("Question:")
         question = str(input());
         if (question in ['', 'exit', 'quit']): break
+
+        print ("Indirect Answer:")
         answer = str(input());
         if (answer in ['', 'exit', 'quit']): break
-        print (compare_sentences(question, answer, print_arg=args.p))
+
+        print("Direct Answer:")
+        print (compare_sentences(question, answer, print_arg=args.p)+"\n")
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
